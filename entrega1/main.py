26a61809de52460b2f9962322c747cd1409b5fff
dmonterog2018 <79135355+dmonterog2018@users.noreply.github.com>
2023-10-19 00:12:11 +0200

    Add files via upload

--- entrega1/main.py
+++ entrega1/main.py
@@ -11,19 +11,22 @@ def main():
     input('Turno del Jugador 1. Pulsa intro para comenzar el juego...')
     j1 = Jugador('nombre')
     j1.crear_equipo('nombre')
+    j1.posicionar_equipo('nombre')
     input('Jugador 1, ha terminado su turno')
     limpiar_terminal()
 
     input('Turno del Jugador 2. Pulsa intro para comenzar su turno...')
     j2 = Jugador('nombre')
     j2.crear_equipo('nombre')
+    j2.posicionar_equipo('nombre')
     input('Jugador 2, ha terminado su turno. Pulse intro.')
     limpiar_terminal()
 
     final = False
     while not final:
         input('Turno del Jugador 1. Pulsa intro para comenzar')
-
+        j1.realizar_accion()
+        j1.informe1()
         if final:
             print('****** EL JUGADOR 1 HA GANADO LA PARTIDA! ********')
             return 0
@@ -31,7 +34,8 @@ def main():
         limpiar_terminal()
 
         input('Turno del Jugador 2. Pulsa intro para comenzar')
-
+        j2.realizar_accion()
+        j2.informe1()
         if final:
             print('****** EL JUGADOR 2 HA GANADO LA PARTIDA! ********')
             return 0
