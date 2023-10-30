db15d4de1255ea0bb8636af086c173506810bec8
DanielBerganciano <121406536+DanielBerganciano@users.noreply.github.com>
2023-10-30 17:10:04 +0100

    Add files via upload

---
+++ entrega1/0. P. Info II/main.py
@@ -0,0 +1,50 @@
+from utils import limpiar_terminal
+from jugador import Jugador
+
+#Funcion main principal que maneja el juego
+def main():
+    print('Bienvenidos a Tactical Battle. Iniciemos tu aventura en el juego!\n')
+    #Jugador 1
+    input('Turno del Jugador 1. Pulsa intro para comenzar el juego...')
+    j1 = Jugador()
+    j1.crear_equipo()
+    j1.posicionar_equipo()
+    input('Jugador 1, ha terminado su turno')
+    limpiar_terminal()
+
+    #Jugador 2
+    input('Turno del Jugador 2. Pulsa intro para comenzar su turno...')
+    j2 = Jugador()
+    j2.crear_equipo()
+    j2.posicionar_equipo()
+    input('Jugador 2, ha terminado su turno. Pulse intro.')
+    limpiar_terminal()
+
+    #Establecemos los oponentes
+    j1.set_oponente(j2)
+    j2.set_oponente(j1)
+
+    #Establecemos el final como False
+    final = False
+
+    #Creamos un bucle que maneje el juego hasta que final = TRUE
+    while not final:
+        input('Turno del Jugador 1. Pulsa intro para comenzar')
+        final = j1.turno1()
+
+        if final:
+            print('****** EL JUGADOR 1 HA GANADO LA PARTIDA! ********')
+            return 0
+        input('Jugador 1, pulse intro para terminar su turno')
+        limpiar_terminal()
+
+        input('Turno del Jugador 2. Pulsa intro para comenzar')
+        final = j2.turno1()
+        if final:
+            print('****** EL JUGADOR 2 HA GANADO LA PARTIDA! ********')
+            return 0
+        input('Jugador 2, pulse intro para terminar su turno')
+        limpiar_terminal()
+
+if __name__ == '__main__':
+    main()
---
+++ entrega1/0. P. Info II/utils.py
@@ -0,0 +1,42 @@
+#Funcion para limpiar el terminal
+def limpiar_terminal():
+    print(chr(27) + "[2J")
+
+#Funcion para validar una celda
+def validar_celda(celda, max_col,max_row) -> bool:
+    if len(celda) > 2 or len(celda) <= 1: #Comprobamos el formato de celdas (B2)
+        print('No es el formato correcto, intentalo otra vez')
+        return False
+    else:
+        if celda[0] <= max_col and int(celda[1]) <= max_row: #Si el formato es correcto y esta dentro del rango
+            return True
+        else: #Si el rango es mayor al establecido FALSE
+            print("Celda fuera de rango, intentalo otra vez")
+            return False
+        
+#Funcion para comrpobar si la celda esta ocupada
+def comprobar_celda_disponible(celda,equipo) -> bool:
+    for i in equipo: #Recorremos la lista equipo
+        for t in i.values(): #Recorremos los valores del diccionario de la lista equipo
+            if t == celda: #Comrporbamos si la celda esta ocupada por otro miembro del equipo
+                print('La celda elegida ya esta ocupada por otro miembro. Seleccione otra por favor')
+                return False
+    else:
+        return True
+
+#Funcion para validar celdas al mover un personaje
+def validar_celda_contigua(celda1,celda2) -> bool:
+    max_col = 'd'
+    max_row = 4
+    if not validar_celda(celda2, max_col,max_row): #Si la funcion validar_celda no es True, devolver FALSE y volver a validar_celda
+        return False
+    #Verificar si celda1 y celda2 son adyacentes
+    if ((celda1[0] == celda2[0] and abs(int(celda1[1]) - int(celda2[1])) == 1) or (celda1[1] == celda2[1] and abs(ord(celda1[0]) - ord(celda2[0]))== 1)):
+        #Columna: ((celda1[0] == celda2[0] and abs(int(celda1[1]) - int(celda2[1])) == 1) comprueba que la celda de la columna es la misma y que la diferencia del valor de la fila es 1
+        #Fila: (celda1[1] == celda2[1] and abs(ord(celda1[0]) - ord(celda2[0]))== 1)) comprueba que la celda de la fila es la misma y que la diferencia del valor de la columna (ASCII) es 1
+        #Si una de las dos condiciones se cumple, significa que la celda es adyacente
+        print('Movimiento efectueado')
+        return True
+    else: #Si ninguna de las condiciones se cumple, la celda no es adyacente y devolvemos FALSE
+        print('La celda a la que intentas moverte no es contigua. Intentalo otra vez')
+        return False
